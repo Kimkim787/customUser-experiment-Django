19a64b5546e8ed30f6d5a74d4ce233daecb26593
Kimkim787 <alcantaraclement@gmail.com>
2024-05-05 19:25:27 +0800

    CUSTOM USER MODEL SUCCESSFUL

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles',
 ]
+
+AUTH_USER_MODEL = "usertest.CustomUser"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
